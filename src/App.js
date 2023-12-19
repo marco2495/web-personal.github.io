17462cbd8c5224b476ca814c13c8f9ae57d475f2
marco2495 <mrcypher_m@protonmail.com>
2023-12-18 18:06:54 -0600

    Verificar el error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import background from './svg_para_el_fondo.svg';  // Asegúrate de proporcionar la ruta correcta
-function App() {
-  const backgroundStyle = {
-    backgroundImage: `url(${background})`,
-    opacity: "3%",
-  }
 
+function App() {
+  
   return (
     <div className="App" >
-      <header className="App-header" style= {backgroundStyle}>
+      <header className="App-header">
+        Hola
       </header>
     </div>
   );
