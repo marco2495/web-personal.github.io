6c7f6255ffc070cee00cc8890d788dbec57f07ed
marco2495 <mrcypher_m@protonmail.com>
2023-12-21 16:57:12 -0600

    comit del navBertoical para que fuincione

--- src/nav-bar-vertical.js
+++ src/nav-bar-vertical.js
@@ -25,7 +25,7 @@ const Navbar = () => {
                             
                         </li>
                         <li>
-                            <a className='group flex items-center py-3 active' href='www.linkedin.com/in/marco845062237'>
+                            <a className='group flex items-center py-3 active' href='https://www.linkedin.com/in/marco845062237/'>
                                 <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
                                 <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'></span>
                                 Experiencia
